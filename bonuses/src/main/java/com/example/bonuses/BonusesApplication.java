cd09652182589b349cf191f4cb1ef9735fde1193
CristobalTorresU <cristobal.torres.u@usach.cl>
2024-05-16 11:11:15 -0400

    RestTemplate

--- bonuses/src/main/java/com/example/bonuses/BonusesApplication.java
+++ bonuses/src/main/java/com/example/bonuses/BonusesApplication.java
@@ -2,8 +2,10 @@ package com.example.bonuses;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class BonusesApplication {
 
 	public static void main(String[] args) {
