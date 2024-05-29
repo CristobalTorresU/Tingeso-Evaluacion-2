79f826faf0605ce8723572a926ee0a6ddbec56f7
CristobalTorresU <cristobal.torres.u@usach.cl>
2024-05-29 03:44:01 -0400

    frontend

--- deployments.sh
+++ deployments.sh
@@ -15,9 +15,8 @@ kubectl apply -f bonuses-db-deployment-service.yaml
 kubectl apply -f config-server-deployment-service.yaml
 kubectl apply -f eureka-server-deployment-service.yaml
 kubectl apply -f gateway-server-deployment-service.yaml
-
-#kubectl apply -f vehicles-deployment-service.yaml
-#kubectl apply -f repairs-deployment-service.yaml
-#kubectl apply -f repairs-list-deployment-service.yaml
-#kubectl apply -f reports-deployment-service.yaml
-#kubectl apply -f bonuses-deployment-service.yaml
+kubectl apply -f vehicles-deployment-service.yaml
+kubectl apply -f repairs-deployment-service.yaml
+kubectl apply -f repairs-list-deployment-service.yaml
+kubectl apply -f reports-deployment-service.yaml
+kubectl apply -f bonuses-deployment-service.yaml
